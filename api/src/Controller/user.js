76f64ec38a6f4f107bba37384c3deb79d6e2dd8c
Brandon <brandonhernandez_10@outlook.es>
2021-08-04 10:14:40 -0600

    Allow string

--- api/src/Controller/user.js
+++ api/src/Controller/user.js
@@ -52,7 +52,7 @@ const signUp = async (req, res) => {
     confirm: Joi.string().required().valid(Joi.ref('password')),
     email: Joi.string().min(5).required(),
     name: Joi.string().min(3).required(),
-    phone: Joi.number(),
+    phone: Joi.string(),
     workplace: Joi.string(),
     residence: Joi.string().required(),
     church: Joi.string(),
