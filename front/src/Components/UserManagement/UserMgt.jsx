81ecc4d5d96f1459d03926f75a397814ddd06251
Andrea Amaya <andrea.a@firstlight.ai>
2021-08-07 21:34:40 -0600

    Arregle errores de render

--- front/src/Components/UserManagement/UserMgt.jsx
+++ front/src/Components/UserManagement/UserMgt.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
@@ -13,64 +12,46 @@ import './UserMgt.css'
 
 const UserMgt = () => {
   const [userInfo, setUserInfo] = useState([])
-  const [update, setUpdate] = useState()
 
   const getUsersInfo = async () => {
-    await fetch('http://localhost:3001/user', {
+    const data = await fetch('http://localhost:3001/user', {
       method: 'GET',
-    }).then((res) => res.json()).then((data) => setUserInfo(data))
+    }).then((res) => res.json()).then((res) => res)
+
+    setUserInfo(data)
   }
 
-  const refreshInfo = () => {
-    fetch('http://localhost:3001/user', {
-      method: 'GET',
-    }).then((res) => res.json()).then((data) => setUserInfo(data))
-  }
-
-  const deletUser = (username) => {
-    fetch(`http://localhost:3001/user/${username}`, {
+  const deleteUser = async (username) => {
+    await fetch(`http://localhost:3001/user/${username}`, {
       method: 'DELETE',
     })
-      .then((res) => res.json())
-      .then((res) => console.log(res))
 
-    refreshInfo()
+    getUsersInfo()
   }
 
-  const activateUser = (user) => {
+  const activateUser = async (user) => {
     const data = {
       username: user,
     }
-    fetch('http://localhost:3001/user/activate', {
+    await fetch('http://localhost:3001/user/activate', {
       method: 'PUT',
       body: JSON.stringify(data),
       headers: { 'Content-type': 'application/json' },
     })
-      .then((res) => console.log(res.status))
-      .catch((e) => console.error('Error', e))
-      .then((out) => {
-        console.log(out)
-      })
-
-    refreshInfo()
+    getUsersInfo()
   }
 
-  const deactivateUser = (user) => {
+  const deactivateUser = async (user) => {
     const data = {
       username: user,
     }
-    fetch('http://localhost:3001/user/desactivate', {
+    await fetch('http://localhost:3001/user/desactivate', {
       method: 'PUT',
       body: JSON.stringify(data),
       headers: { 'Content-type': 'application/json' },
     })
-      .then((res) => console.log(res.status))
-      .catch((e) => console.error('Error', e))
-      .then((out) => {
-        console.log(out)
-      })
 
-    refreshInfo()
+    getUsersInfo()
   }
 
   useEffect(() => {
@@ -88,39 +69,27 @@ const UserMgt = () => {
           <h1 className="titulos estado">Estado</h1>
         </div>
         <div className="users-container">
-          {
-            userInfo.length > 0
-              ? userInfo.map((value, index) => {
-                if (value.active) {
-                  return (
-                    <div className="usersmanaged" key={index}>
-                      <h3 className="titulos">{value.name}</h3>
-                      <h3 className="titulos">{value.username}</h3>
-                      <h5 className="titulos">{value.email}</h5>
-                      <div className="imagen">
-                        <img className="mgtimage" src={Active} alt="active" onClick={() => deactivateUser(value.username)} />
-                      </div>
-                      <div className="trash-container">
-                        <img className="mgtimage" src={Trash} alt="trash" onClick={() => deletUser(value.username)} />
-                      </div>
-                    </div>
-                  )
-                }
-                return (
+          { userInfo.length > 0
+            ? (
+              <>
+                { userInfo.map((value, index) => (
                   <div className="usersmanaged" key={index}>
                     <h3 className="titulos">{value.name}</h3>
                     <h3 className="titulos">{value.username}</h3>
                     <h5 className="titulos">{value.email}</h5>
                     <div className="imagen">
-                      <img className="mgtimage" src={Inactive} alt="inactive" onClick={() => activateUser(value.username)} />
+                      { value.active
+                        ? <img className="mgtimage" src={Active} alt="active" onClick={() => deactivateUser(value.username)} />
+                        : <img className="mgtimage" src={Inactive} alt="inactive" onClick={() => activateUser(value.username)} />}
                     </div>
                     <div className="trash-container">
-                      <img className="mgtimage" src={Trash} alt="trash" onClick={() => deletUser(value.username)} />
+                      <img className="mgtimage" src={Trash} alt="trash" onClick={() => deleteUser(value.username)} />
                     </div>
                   </div>
-                )
-              }) : ''
-          }
+                )) }
+              </>
+            )
+            : '' }
         </div>
       </div>
     </div>
