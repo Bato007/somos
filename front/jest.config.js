fc18df9c49df9cc472589f7f08ea46543d9e1d90
Andrea Amaya <andrea.a@firstlight.ai>
2021-08-19 07:38:13 -0600

    jest esta tirando errores feos :(

--- front/jest.config.js
+++ front/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    '\\.css$': '<rootDir>/src/__mocks__/css.mock.js',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
   },
 }
--- front/src/Components/List/List.stories.jsx
+++ front/src/Components/List/List.stories.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import List from './List'
-import './List'
+import './List.css'
 
 export default {
   title: 'List',
