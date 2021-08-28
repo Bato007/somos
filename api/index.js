ab7f6551f20573ad1d8533d66014bb1822568f4f
Bato007 <brandonhernandez_10@outlook.es>
2021-08-28 00:05:17 -0600

    Swagger Documentation

---
+++ api/index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const cors = require('cors')
+const swaggerUI = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
+
+const app = express()
+const PORT = 3001
+
+const adminRouter = require('./src/Routers/admin')
+const authenticationRouter = require('./src/Routers/authentication')
+const resourcesRouter = require('./src/Routers/resources')
+const announcementRouter = require('./src/Routers/announcement')
+const userRouter = require('./src/Routers/usermanagment')
+
+const { authorizate } = require('./src/Middleware/authorization')
+
+app.use(cors())
+app.use(express.json())
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'SOMOS API',
+      version: '1.0.0',
+      description: 'Documentacion del api',
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`,
+      },
+    ],
+  },
+  apis: ['./src/Routers/*.js', './src/Routers/admin/*.js'],
+}
+
+const specs = swaggerJsDoc(options)
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs))
+
+// app.use('/', authorizate)
+
+// Todas las rutas validas para administrador
+app.use('/admin', adminRouter)
+
+// Todas las rutas validas para cualquiera
+app.use('/authentication', authenticationRouter)
+app.use('/resources', resourcesRouter)
+app.use('/announcements', announcementRouter)
+app.use('/user', userRouter)
+
+// Ahora se tira error de que no encontro la ruta
+app.use((req, res) => {
+  res.status(404).json({ message: 'NOT FOUND' })
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
--- api/src/Routers/announcement.js
+++ api/src/Routers/announcement.js
@@ -9,54 +9,162 @@ const {
 const router = express.Router()
 
 /**
- * Los objetos para estas rutas tienen la siguiente
- * estrucutra:
- * {
- *  id: 'ejemplo',
- *  contact: 'ejemplo',
- *  phone: 12315123,
- *  email: 'ejemplo@gmail',
- *  title: 'ejemplo',
- *  description: 'ejemplo',
- *  toDate: '2021-04-01',
- *  published: true,
- *  type: 'ejemplo'
- * }
+ * @swagger
+ * tags:
+ *   name: Anuncios
+ *   description: Los anuncios que maneja el API
  */
 
 /**
- * Obtiene todos los anuncios de la plataforma
+ * @swagger
+ * components:
+ *  schemas:
+ *    Anuncio:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: string
+ *          description: Identificador del anuncio
+ *        contact:
+ *          type: string
+ *          description: Es la persona a quien se contactan
+ *        phone:
+ *          type: integer
+ *          description: El numero de celular de la persona
+ *        email:
+ *          type: string
+ *          description: Es el correo al que se pueden contactar
+ *        title:
+ *          type: string
+ *          description: Es el titulo del anuncio
+ *        description:
+ *          type: string
+ *          description: Es el cuerpo del anuncio, que es lo que ofrece
+ *        toDate:
+ *          type: date
+ *          description: Hasta que fecha es valido el anuncio
+ *        published:
+ *          type: integer
+ *          description: indica el estado del anuncio = no publicado (0), publicado (1)
+ */
+
+/**
+ * @swagger
+ * /announcements:
+ *  get:
+ *    summary: Retorna todos los anuncios
+ *    tags: [Anuncios]
+ *    responses:
+ *      200:
+ *        description: Obtiene todos los anuncios
+ *        content:
+ *          application/json:
+ *            type: array
+ *            items:
+ *              $ref: '#/components/schemas/Anuncio'
  */
 router.get('/', (req, res) => getAll(req, res))
 
 /**
- * Obtiene todos los anuncios de la categoria help
+ * @swagger
+ * /announcements/help:
+ *  get:
+ *    summary: Retorna todos los anuncios de los brindadores de servicio
+ *    tags: [Anuncios]
+ *    responses:
+ *      200:
+ *        description: Obtiene todos los anuncios
+ *        content:
+ *          application/json:
+ *            type: array
+ *            items:
+ *              $ref: '#/components/schemas/Anuncio'
  */
 router.get('/help', (req, res) => getAllHelp(req, res))
 
 /**
- * Obtiene los anuncios de help no publicados
+ * @swagger
+ * /announcements/help/nonpublished:
+ *  get:
+ *    summary: Retorna todos los anuncios de los brindadores de servicio no publicados
+ *    tags: [Anuncios]
+ *    responses:
+ *      200:
+ *        description: Obtiene los anuncios con published 0
+ *        content:
+ *          application/json:
+ *            type: array
+ *            items:
+ *              $ref: '#/components/schemas/Anuncio'
  */
 router.get('/help/nonpublished', (req, res) => getNonPublishedhHelp(req, res))
 
 /**
- * Obtiene los anuncios de help publicados
+ * @swagger
+ * /announcements/help/published:
+ *  get:
+ *    summary: Retorna todos los anuncios de los brindadores de servicio publicados
+ *    tags: [Anuncios]
+ *    responses:
+ *      200:
+ *        description: Obtiene los anuncios con published 1
+ *        content:
+ *          application/json:
+ *            type: array
+ *            items:
+ *              $ref: '#/components/schemas/Anuncio'
  */
 router.get('/help/published', (req, res) => getPublishedhHelp(req, res))
 
 /**
- * Obtiene todos los anuncios de la categoria home
+ * @swagger
+ * /announcements/home:
+ *  get:
+ *    summary: Retorna todos los anuncios de los hogares / iglesias
+ *    tags: [Anuncios]
+ *    responses:
+ *      200:
+ *        description: Obtiene todos los anuncios
+ *        content:
+ *          application/json:
+ *            type: array
+ *            items:
+ *              $ref: '#/components/schemas/Anuncio'
  */
 router.get('/home', (req, res) => getAllHomes(req, res))
 
 /**
-  * Obtiene los anuncios de home no publicados
-  */
+ * @swagger
+ * /announcements/home/nonpublished:
+ *  get:
+ *    summary: Retorna todos los anuncios de los hogares / iglesias no publicados
+ *    tags: [Anuncios]
+ *    responses:
+ *      200:
+ *        description: Obtiene los anuncios con published 0
+ *        content:
+ *          application/json:
+ *            type: array
+ *            items:
+ *              $ref: '#/components/schemas/Anuncio'
+ */
 router.get('/home/nonpublished', (req, res) => getNonPublishedhHome(req, res))
 
 /**
-  * Obtiene los anuncios de home publicados
-  */
+ * @swagger
+ * /announcements/home/published:
+ *  get:
+ *    summary: Retorna todos los anuncios de los hogares / iglesias publicados
+ *    tags: [Anuncios]
+ *    responses:
+ *      200:
+ *        description: Obtiene los anuncios con published 1
+ *        content:
+ *          application/json:
+ *            type: array
+ *            items:
+ *              $ref: '#/components/schemas/Anuncio'
+ */
 router.get('/home/published', (req, res) => getPublishedhHome(req, res))
 
 module.exports = router
