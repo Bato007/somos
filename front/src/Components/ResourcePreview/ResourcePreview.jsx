5e4625351cfbfd3590a276b7c1c7a201736480b0
Andrea Amaya <andrea.a@firstlight.ai>
2021-08-29 10:38:23 -0600

    Resources previews

--- front/src/Components/ResourcePreview/ResourcePreview.jsx
+++ front/src/Components/ResourcePreview/ResourcePreview.jsx
@@ -9,7 +9,9 @@ import previewPdf from '../../static/imagesResourcesPreview/pdf.png'
 import previewDoc from '../../static/imagesResourcesPreview/doc.png'
 import previewPpt from '../../static/imagesResourcesPreview/ppt.png'
 import previewVideo from '../../static/imagesResourcesPreview/video.png'
-
+import previewImage from '../../static/imagesResourcesPreview/image.png'
+import previewXlxs from '../../static/imagesResourcesPreview/xlsx.png'
+import previewDefault from '../../static/imagesResourcesPreview/default.png'
 /**
  * Clase para mostrar la preview del recurso dependiendo de la extension del archivo
  * Donde las extensiones tomadas en cuenta son: docs, pdf, videos, ppt
@@ -26,14 +28,22 @@ const ResourcePreview = ({ availableResources }) => {
 
   // Metodo para colocar la imagen correspondiente del recurso dependiendo la extension
   const getPreview = (extension) => {
-    if (extension === 'pdf') {
+    const ext = extension.toLowerCase()
+
+    if (ext.match(/(pdf)/)) {
       return previewPdf
-    } if (extension === 'ppt') {
+    } if (ext.match(/(ppt|pptx)/)) {
       return previewPpt
-    } if (extension === 'doc') {
+    } if (ext.match(/(doc|word|txt|docx)/)) {
       return previewDoc
+    } if (ext.match(/(jpg|jpeg|png|gif|eot|otf|webp|svg)/)) {
+      return previewImage
+    } if (ext.match(/(mp4|webm|wav|mp3|m4a|aac|oga)/)) {
+      return previewVideo
+    } if (ext.match(/(xml|xls|xlt|xlsx|xlsm|xlbs|xltx|excel)/)) {
+      return previewXlxs
     }
-    return previewVideo
+    return previewDefault
   }
 
   return (
