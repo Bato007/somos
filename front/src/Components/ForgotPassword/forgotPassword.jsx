5bb40a93ed0a29c6daeddb81c3a681cb6ff50fa0
lauratamathWD <tam19365@uvg.edu.gt>
2021-10-03 00:33:46 -0600

    forgotPassword

--- front/src/Components/ForgotPassword/forgotPassword.jsx
+++ front/src/Components/ForgotPassword/forgotPassword.jsx
@@ -1,19 +1,51 @@
-/* eslint-disable max-len */
 import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import Error from '../Error/Error'
+import Button from '../Button/Button'
 import Input from '../Input/Input'
 import './forgotPassword.css'
 
 const ForgotPassword = () => {
-  const [accountInfo, setAccountInfo] = useState({
-    email: '',
-  })
-  const handleManageAccount = (event) => {
-    setAccountInfo({
-      ...accountInfo,
+  const history = useHistory()
+  const [account, setAccount] = useState({ email: '' })
+  const [error, setError] = useState('')
+
+  const handleChange = (event) => {
+    setAccount({
+      ...account,
       [event.target.name]: event.target.value,
     })
   }
 
+  const existingAccounts = async () => {
+    const { email } = account
+    let status
+    const json = await fetch('http://localhost:3001/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        somoskey: `${localStorage.getItem('somoskey')}`,
+      },
+      body: JSON.stringify({ email }),
+    }).then((res) => {
+      status = res.status
+      return res.json()
+    })
+
+    if (status === 200) {
+      setError('')
+      const { somoskey } = json
+      localStorage.setItem('email', email)
+      localStorage.setItem('somoskey', somoskey)
+      if (!json.isSOMOS) {
+        history.push('/client')
+      } else if (json.isSOMOS) {
+        history.push('/admin')
+      }
+    } else if (json.username === 'ERROR 101') {
+      setError('Correo electrónico incorrecto')
+    }
+  }
   return (
     <div className="forgotPass">
       <div id="forgotPass">
@@ -22,10 +54,13 @@ const ForgotPassword = () => {
           Ingresa tu correo electrónico y te enviaremos el
           código para restablecer la contraseña
         </p>
-        <Input name="email" type="text" onChange={handleManageAccount} />
-        {/* <Input value={mail} className="InputCreate" type="text" name="email" placeholder="Correo electrónico" onChange={(event) => setMail(event.target.value)} /> */}
+        <Input name="email" type="text" placeholder="Correo electrónico" onChange={handleChange} onEnter={existingAccounts} />
+        <div className="buttonsFp">
+          <Button id="returnToSignIn" name="Cancel" onClick={() => history.replace('/')} />
+          <Button id="SignIn" name="Continue" onClick={existingAccounts} />
+        </div>
+        <Error error={error} />
       </div>
-
     </div>
   )
 }
