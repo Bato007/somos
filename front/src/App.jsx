b8dc7c0e3400756bd14a12dbbe7f772851a3159b
lauratamathWD <tam19365@uvg.edu.gt>
2021-10-03 02:07:59 -0600

    serviceProvider layout

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Login from './Components/Login/Login'
 import ForgotPassword from './Components/ForgotPassword/forgotPassword'
 import TokenForgetPass from './Components/ForgotPassword/forgotPasswordVerify'
+import serviceProvider from './Components/ServiceProvider/serviceProvider'
 import ClientRoutes from './ClientRoutes'
 import AdminRoutes from './AdminRoutes'
 
@@ -15,6 +16,7 @@ const App = () => (
         <Route path="/admin" component={AdminRoutes} />
         <Route path="/forgotPasswordToken" component={TokenForgetPass} />
         <Route path="/forgotPassword" component={ForgotPassword} />
+        <Route path="/serviceProvider" component={serviceProvider} />
         <Route path="/" component={Login} />
       </Switch>
     </Router>
