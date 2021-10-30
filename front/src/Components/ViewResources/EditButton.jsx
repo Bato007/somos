fe2a4f26dae9c58ea8cd891e8d647b31b6d018b2
Levi Tamath <tam19365@uvg.edu.gt>
2021-10-29 22:54:22 -0600

    edit resource

--- front/src/Components/ViewResources/EditButton.jsx
+++ front/src/Components/ViewResources/EditButton.jsx
@@ -7,19 +7,21 @@ import apiURL from '../fetch'
 
 const EditButton = ({ resourceId }) => {
   const [editInfo, setEditInfo] = useState({
-    title: ' ', description: ' ', available: ' ', tags: ' ', users: ' ',
+    title: ' ', description: ' ', available: ' ', tags: ' ', users: ' ', category: ' ',
   })
   let { title } = editInfo
   let description = editInfo.title
   let { available } = editInfo
   let { tags } = editInfo
   let { users } = editInfo
+  let category = editInfo.categories
   const [modal, setModal] = useState(false)
   const [titleUpd, setTitleUpd] = useState('')
   const [descUpd, setDescUpd] = useState('')
   const [dateUpd, setDateUpd] = useState('')
   const [tagUpd, setTagUpd] = useState('')
   const [userUpd, setUserUpd] = useState('')
+  const [categoriesUpd, setCategoriesUpd] = useState('')
 
   const [resInfo, setResInfo] = useState({ })
   // Fetch para obtener la informacion del recurso seleccionado
@@ -65,7 +67,7 @@ const EditButton = ({ resourceId }) => {
   const actualDate = dateF()
 
   const EditSource = async () => {
-    if (title !== '' || description !== '' || available !== '' || tags !== '' || users !== '') {
+    if (title !== '' || description !== '' || available !== '' || tags !== '' || users !== '' || category !== '') {
       if (editInfo.title === '') {
         title = titleUpd
       }
@@ -93,11 +95,17 @@ const EditButton = ({ resourceId }) => {
       if (editInfo.users !== '') {
         users = resInfo.users
       }
+      if (editInfo.category === '') {
+        category = categoriesUpd
+      }
+      if (editInfo.category !== '') {
+        category = resInfo.category
+      }
       if (editInfo.title !== '') {
         title = resInfo.title
         const id = resourceId
         // const { tags } = resInfo
-        const category = resInfo.categories
+        // const category = resInfo.categories
         // const { users } = resInfo
         const { filename } = resInfo
         await fetch(`${apiURL}/admin/resources`, {
@@ -111,7 +119,7 @@ const EditButton = ({ resourceId }) => {
             title: titleUpd || title,
             description: descUpd || description,
             tags: tagUpd || tags,
-            category,
+            category: categoriesUpd || category,
             users: userUpd || users,
             date: dateUpd || available,
             filename,
@@ -137,31 +145,18 @@ const EditButton = ({ resourceId }) => {
     setDateUpd(document.getElementById('dateModify').value)
   }
 
-  // Necesito ver si esta funcion funciona como debe
-  // const handleTagsChange = () => () => {
-  //   setTagUpd(document.getElementById('tagModify').value)
-  // }
-
   const body = (
     <div id="editResource">
       <div className=" makeStyles-modal makeStyles-modal-1">
         <h2>Edición del recurso</h2>
         <TextField id="titleChangeVR" label={resInfo.title} onChange={() => handleTitleChange()} />
         <TextField id="descriptionChangeVR" label={resInfo.description} onChange={() => handleDescChange()} />
-        <h3>Estas son las tags actuales: </h3>
-        <div className="otherLine">
-          {resInfo.tags !== undefined
-            ? resInfo.tags.map((tag) => <p>{tag}</p>)
-            : null}
-        </div>
-        <SearchBarTo showSimilarTo setSimilarTo={setTagUpd} />
-        <h3>Estos sons los usuarios actuales: </h3>
-        <div className="otherLine">
-          {resInfo.users !== undefined
-            ? resInfo.users.map((tag) => <p>{tag}</p>)
-            : null}
-        </div>
-        <SearchBarTo setAccounts={setUserUpd} />
+        <SearchBarTo showSimilarTo lastResult={resInfo.tags} setSimilarTo={setTagUpd} />
+        <SearchBarTo
+          lastResult={resInfo.categories}
+          setAccounts={setUserUpd}
+          setCategories={setCategoriesUpd}
+        />
         <TextField id="dateModify" type="date" defaultValue={resInfo.available} label={resInfo.available} inputProps={{ min: actualDate }} onChange={() => handleDateChange()} />
         <div className="buttonsEdit">
           <button type="button" className="closeButton" onClick={() => abrirCerrarModal()}>cancel</button>
