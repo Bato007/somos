a3dad6c71c4120ab883ecd80765b737b4079e257
Andrea Amaya <andrea.a@firstlight.ai>
2021-11-01 00:34:52 -0600

    Mensajes en manage account

--- front/src/Components/ManageAccount/ManageAccount.jsx
+++ front/src/Components/ManageAccount/ManageAccount.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import swal from 'sweetalert'
 import Input from '../Input/Input'
 import Button from '../Button/Button'
 import SearchBarTo from '../SearchbarTo/SearchbarTo'
@@ -9,8 +11,8 @@ const ManageAccount = () => {
   const [accountInfo, setAccountInfo] = useState({
     username: '', email: '', tel: '', password: '', confirmPassword: '', address: '', categories: [],
   })
-  // eslint-disable-next-line no-unused-vars
   const [categories, setCategories] = useState([])
+  const history = useHistory()
 
   // Se obtiene la info del usuario y se coloca en el estado
   const getUserInfo = async () => {
@@ -62,11 +64,63 @@ const ManageAccount = () => {
         body: JSON.stringify(data),
         headers: { 'Content-type': 'application/json', somoskey: `${localStorage.getItem('somoskey')}` },
       }).then((res) => {
-      console.log(res.status)
-      return res.json()
-    }).then((res) => (res))
-      .catch((e) => console.error('Error', e))
+      if (res.status !== 200) {
+        return res?.json()
+      }
+      return undefined
+    }).then((res) => (res)).catch((e) => console.error('Error', e))
     console.log(response)
+    const result = response?.message
+    if (result) {
+      if (result.includes('100')) {
+        swal({
+          title: 'Por favor, termina de llenar todos los campos',
+          icon: 'error',
+          buttons: ['Aceptar'],
+        })
+      } else if (result.includes('101')) {
+        swal({
+          title: 'Por favor, revisa que el correo/celular sean válidos',
+          icon: 'error',
+          buttons: ['Aceptar'],
+        })
+      } else if (result.includes('102')) {
+        swal({
+          title: 'Oops! Necesitas al menos una categoría',
+          icon: 'warning',
+          buttons: ['Aceptar'],
+        })
+      } else if (result.includes('105')) {
+        swal({
+          title: 'Oops! Revisa que tu contraseña cumpla con los requisitos pedidos',
+          text: 'La contraseña debe de tener un mínimo de 8 letras: un número, una letra minúscula, una letra mayúscula, un caracter especial',
+          icon: 'warning',
+          buttons: ['Aceptar'],
+        })
+      } else if (result.includes('106')) {
+        swal({
+          title: 'Oops! Tus contraseñas no coinciden',
+          icon: 'error',
+          buttons: ['Aceptar'],
+        })
+      } else if (result.includes('107')) {
+        swal({
+          title: 'Oops! Tu residencia no parece ser válida',
+          icon: 'error',
+          buttons: ['Aceptar'],
+        })
+      }
+    } else {
+      swal({
+        title: '¡Tu cuenta se ha actualizado con exito!',
+        icon: 'success',
+      }).then((res) => {
+        if (res) {
+          const path = window.location.href.split('/')[3]
+          history.push(`/${path}`)
+        }
+      })
+    }
   }
 
   // Leyendo el valor actual de los field
