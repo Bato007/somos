48752c5f57c8005345fb0785f751076e2f1ff205
Levi Tamath <tam19365@uvg.edu.gt>
2021-11-14 12:09:57 -0600

    mensajes

--- front/src/Components/ForgotPassword/forgotPassword.jsx
+++ front/src/Components/ForgotPassword/forgotPassword.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
-import apiURL from '../fetch'
-import Error from '../Error/Error'
+// import Error from '../Error/Error'
 import Button from '../Button/Button'
 import Input from '../Input/Input'
 import './forgotPassword.css'
@@ -9,7 +8,7 @@ import './forgotPassword.css'
 const ForgotPassword = () => {
   const history = useHistory()
   const [account, setAccount] = useState({ email: '' })
-  const [error, setError] = useState('')
+  // const [error, setError] = useState('')
 
   const handleChange = (event) => {
     setAccount({
@@ -18,32 +17,33 @@ const ForgotPassword = () => {
     })
   }
 
-  const existingAccounts = async () => {
-    const { email } = account
-    let status
-    const json = await fetch(`${apiURL}/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        somoskey: `${localStorage.getItem('somoskey')}`,
-      },
-      body: JSON.stringify({ email }),
-    }).then((res) => {
-      status = res.status
-      return res.json()
-    })
+  const requestReset = async () => {
+    history.push('/forgotPassword/token')
+    // const { email } = account
+    // let status
+    // const json = await fetch(`${apiURL}/login`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     somoskey: `${localStorage.getItem('somoskey')}`,
+    //   },
+    //   body: JSON.stringify({ email }),
+    // }).then((res) => {
+    //   status = res.status
+    //   return res.json()
+    // })
 
-    if (status === 200) {
-      setError('')
-      const { somoskey } = json
-      localStorage.setItem('email', email)
-      localStorage.setItem('somoskey', somoskey)
-      if (!json.isSOMOS) {
-        history.push('/forgotPassword/token')
-      }
-    } else if (json.username === 'ERROR 101') {
-      setError('Correo electrónico incorrecto')
-    }
+    // if (status === 200) {
+    //   setError('')
+    //   const { somoskey } = json
+    //   localStorage.setItem('email', email)
+    //   localStorage.setItem('somoskey', somoskey)
+    //   if (!json.isSOMOS) {
+    //     history.push('/forgotPassword/token')
+    //   }
+    // } else if (json.username === 'ERROR 101') {
+    //   setError('Correo electrónico incorrecto')
+    // }
   }
   return (
     <div className="forgotPass">
@@ -53,12 +53,14 @@ const ForgotPassword = () => {
           Ingresa tu correo electrónico y te enviaremos el
           código para restablecer la contraseña
         </p>
-        <Input name="email" type="text" placeholder="Correo electrónico" onChange={handleChange} onEnter={existingAccounts} />
+        <Input name="email" type="text" placeholder="Correo electrónico" onChange={handleChange} onEnter={requestReset} />
+        {' '}
+        {/* history.push('/forgotPassword/token')} */}
         <div className="buttonsFp">
           <Button id="returnToSignIn" name="Cancelar" onClick={() => history.replace('/')} />
-          <Button id="SignIn" name="Continuar" onClick={() => history.push('/forgotPassword/token')} />
+          <Button id="SignIn" name="Continuar" onClick={requestReset} />
         </div>
-        <Error error={error} />
+        {/* <Error error={error} /> */}
       </div>
     </div>
   )
